78470114e89c22e41ff6e33fd9b380227531b89c
Thiện Nguyễn <thiennguyenminh912@gmail.com>
2022-02-01 00:56:33 +0700

    Implement 404 page and styled

--- src/app.js
+++ src/app.js
@@ -3,35 +3,65 @@ const express = require("express");
 const hbs = require("hbs");
 
 const app = express();
-const publicPath = path.join(__dirname, "../public");
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
 const viewsPath = path.join(__dirname, "../templates/views");
 const partialsPath = path.join(__dirname, "../templates/partials");
 
+// Setup handlebars engine and views location
 app.set("view engine", "hbs");
 app.set("views", viewsPath);
-app.use(express.static(publicPath));
 hbs.registerPartials(partialsPath);
 
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
 app.get("", (req, res) => {
-  res.render("index", { title: "Weather App", name: "Thien Nguyen" });
+  res.render("index", {
+    title: "Weather",
+    name: "Andrew Mead",
+  });
 });
 
 app.get("/about", (req, res) => {
-  res.render("about", { title: "About", name: "Thien Nguyen" });
+  res.render("about", {
+    title: "About Me",
+    name: "Andrew Mead",
+  });
 });
 
 app.get("/help", (req, res) => {
-  res.render("help", { title: "Help", content: "This is the help page." });
+  res.render("help", {
+    helpText: "This is some helpful text.",
+    title: "Help",
+    name: "Andrew Mead",
+  });
 });
 
 app.get("/weather", (req, res) => {
-  res.send({ forcast: "50 degree", location: "HCM" });
+  res.send({
+    forecast: "It is snowing",
+    location: "Philadelphia",
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Andrew Mead",
+    errorMessage: "Help article not found.",
+  });
 });
 
 app.get("*", (req, res) => {
-  res.send("404");
+  res.render("404", {
+    title: "404",
+    name: "Andrew Mead",
+    errorMessage: "Page not found.",
+  });
 });
 
-app.listen(3005, () => {
-  console.log("Server is up on port 3005");
+app.listen(3000, () => {
+  console.log("Server is up on port 3000.");
 });
